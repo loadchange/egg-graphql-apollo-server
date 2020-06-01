6abe1667558ccafdde85f6cfe5dcc16c94dd407d
LoadChange <soocto@gmail.com>
2020-06-01 18:53:18 +0800

    bugfix

---
+++ tools/install.js
@@ -0,0 +1,23 @@
+#!/usr/bin/env node
+"use strict";
+
+const open = require("open");
+const {createServer} = require("http");
+const {resolve} = require("path");
+const {readFileSync} = require("fs");
+
+const file = readFileSync(resolve(__dirname, "../github-dark.user.css"));
+
+const server = createServer((_, res) => {
+  res.setHeader("content-type", "text/css");
+  res.on("close", () => {
+    setTimeout(() => {
+      process.exit(0);
+    }, 4000);
+  });
+  res.end(file);
+});
+
+server.listen(() => {
+  open(`http://localhost:${server.address().port}/github-dark.user.css`);
+});
